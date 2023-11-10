c36a1005ce883ff2ee741f5958f79014f1a3398e
pedrojschmidt <pedro.schmidt@ing.austral.edu.ar>
2023-11-10 12:18:17 -0300

    update stock modal

--- src/pages/admin/AdminPage.js
+++ src/pages/admin/AdminPage.js
@@ -1,8 +1,14 @@
-import {useState} from "react";
+import React, {useState} from "react";
 import Modal from "../../components/Modal";
+import './AdminPage.css';
+import GoBackButton from "../../components/GoBackButton";
 
 const AdminPage = () => {
+
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [addedStock, setAddedStock] = useState(0);
+    // pedirselo al back
+    const [actualStock, setActualStock] = useState(50);
 
     const openModal = () => {
         setIsModalOpen(true);
@@ -17,16 +23,29 @@ const AdminPage = () => {
         closeModal(); // Cierra el modal después de actualizar el stock.
     };
 
+    const handleAddStock = (event) => {
+        setAddedStock(event.target.value);
+    }
+
     return (
         <div className="admin-page">
-            <h1>Panel de Administrador</h1>
-            <button onClick={openModal}>Actualizar Stock</button>
-
+            <GoBackButton />
+            <div className="admin-page-container">
+                <h2>Panel de Administrador</h2>
+                <button className="contained" onClick={openModal}>Actualizar Stock</button>
+            </div>
             <Modal isOpen={isModalOpen} onRequestClose={closeModal}>
                 <h2>Actualizar Stock</h2>
                 {/* Agrega los campos y lógica necesarios para actualizar el stock en el modal */}
-                <button onClick={handleUpdateStock}>Guardar Cambios</button>
-                <button onClick={closeModal}>Cancelar</button>
+                <input
+                    type="number"
+                    placeholder="Contraseña"
+                    value={addedStock}
+                    onChange={handleAddStock}
+                    min={0}
+                    max={200 - actualStock}
+                />
+                <button className="outlined" onClick={handleUpdateStock}>Guardar Cambios</button>
             </Modal>
         </div>
     );
