c2ab2579ac8c61abd8352fd62bb7861acdd4f246
pedrojschmidt <pedro.schmidt@ing.austral.edu.ar>
2023-11-21 21:39:27 -0300

    Buy operas done

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,16 +1,87 @@
 import './HomePage.css'
 import GoBackButton from "../../components/GoBackButton";
-import React from "react";
+import React, {useState} from "react";
+import Modal from "../../components/Modal";
 
 const HomePage = () => {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [originalActualStock, setOriginalActualStock] = useState(50);
+    const [chocolateActualStock, setChocolateActualStock] = useState(20);
+    const [strawberyActualStock, setStrawberyActualStock] = useState(30);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleOriginalPurchase = () => {
+        if (originalActualStock > 0) {
+            setOriginalActualStock(Number(originalActualStock) - Number(1));
+        } else {
+            alert("No hay stock de Opera Original");
+        }
+    };
+
+    const handleChocolatePurchase = () => {
+        if (chocolateActualStock > 0) {
+            setChocolateActualStock(Number(chocolateActualStock) - Number(1));
+        } else {
+            alert("No hay stock de Opera Chocolate");
+        }
+    };
+
+    const handleStrawberyPurchase = () => {
+        if (strawberyActualStock > 0) {
+            setStrawberyActualStock(Number(strawberyActualStock) - Number(1));
+        } else {
+            alert("No hay stock de Opera Frutilla");
+        }
+    };
 
     return(
         <div className="home">
             <GoBackButton />
             <div className="admin-login">
                 <h2>Comprar Operas</h2>
-                <button className="contained">Comprar</button>
+                <button className="contained" onClick={openModal}>Comprar</button>
             </div>
+            <Modal isOpen={isModalOpen} onRequestClose={closeModal}>
+                <h2>Comprar</h2>
+                {/* Agrega los campos y lógica necesarios para actualizar el stock en el modal */}
+                <div className="variety-conatiner">
+                    <div className="variety">
+                        <div className="variety-name" style={{ backgroundColor: '#888' }}>
+                            Opera Original
+                        </div>
+                        <div className="variety-stock">
+                            Stock: {originalActualStock}
+                        </div>
+                        <button className="contained" onClick={handleOriginalPurchase}>Comprar</button>
+                    </div>
+                    <div className="variety">
+                        <div className="variety-name" style={{ backgroundColor: '#482d23' }}>
+                            Opera Chocolate
+                        </div>
+                        <div className="variety-stock">
+                            Stock: {chocolateActualStock}
+                        </div>
+                        <button className="contained" onClick={handleChocolatePurchase}>Comprar</button>
+                    </div>
+                    <div className="variety">
+                        <div className="variety-name" style={{ backgroundColor: '#ce6aa1' }}>
+                            Opera Frutilla
+                        </div>
+                        <div className="variety-stock">
+                            Stock: {strawberyActualStock}
+                        </div>
+                        <button className="contained" onClick={handleStrawberyPurchase}>Comprar</button>
+                    </div>
+                </div>
+            </Modal>
         </div>
     )
 }
